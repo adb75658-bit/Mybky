46b92ea5a435d70632c0f66ee6d544cb5af63e6c
sheejashaji5791-art <sheejashaji5791@gmail.com>
2026-01-12 17:18:30 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1151,6 +1151,15 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('⇚Back', callback_data='start')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
+	    await query.message.edit_text(
+            text="▮▯▯"
+        )
+        await query.message.edit_text(
+            text="▮▮▯"
+        )
+        await query.message.edit_text(
+            text="▮▮▮"
+		)
         await client.edit_message_media(
             query.message.chat.id, 
             query.message.id, 
